e0e966e90ddef2198439f06f582721fd956e4d35
zhangchao <zackability@hotmail.com>
2019-09-20 18:12:30 +0800

    feat(CommandLineUtils): nuget升级，添加检测脚本

---
+++ CommandLineUtilsDemo/checkRun.ps1
@@ -0,0 +1,15 @@
+﻿.\publish_run_client_attr.ps1 *> attr_rt.log
+dotnet.exe run -p .\AttributesCoreRTDemo\AttributesCoreRTDemo.csproj -c release -r win-x64 *> attr.log
+
+.\publish_run_client_attr_adv.ps1 *> adv_attr_rt.log
+dotnet.exe run -p .\AdvancedAttributesCoreRTDemo\AdvancedAttributesCoreRTDemo.csproj -c release -r win-x64 *> adv_attr.log
+
+.\publish_run_client.ps1 *> builder_rt.log
+dotnet.exe run -p .\BuilderApiCoreRTDemo\BuilderApiCoreRTDemo.csproj -c release -r win-x64 -- `
+"subcommand1" `
+    "20" `
+    "hello world vscode" `
+    "-s:haha" `
+    "-r:50" `
+    "-v" `
+    "-a:123" *> builder.log
