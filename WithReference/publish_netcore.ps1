c113bdf729201f913046f35cfa03c5ecacae2227
zhangchao <zackability@hotmial.com>
2020-07-01 10:47:46 +0800

    feat(WithReference): update dotnet to 3.1，Microsoft.DotNet.ILCompiler to 1.0.0-alpha-29030-01

--- WithReference/publish_netcore.ps1
+++ WithReference/publish_netcore.ps1
@@ -1,1 +1,2 @@
-﻿dotnet.exe publish .\ConsoleApp\ConsoleApp.csproj -c release -r win-x64 -f netcoreapp2.1
+﻿dotnet.exe publish .\ConsoleApp\ConsoleApp.csproj -c release -r win-x64 -f netcoreapp3.1
+.\ConsoleApp\bin\release\netcoreapp3.1\win-x64\publish\ConsoleApp.exe
