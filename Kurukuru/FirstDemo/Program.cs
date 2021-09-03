ddde88f5fea1044d07b90d859f5f17c35369116e
zhangchao <zackability@hotmail.com>
2021-09-03 22:57:34 +0800

    feat: add kurukuru

---
+++ Kurukuru/FirstDemo/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Kurukuru;
+
+namespace FirstDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Demo1();
+            AsyncDemo().GetAwaiter().GetResult();
+        }
+
+        static void Demo1()
+        {
+            Spinner.Start("Processing...", () =>
+            {
+                Thread.Sleep(1000 * 3);
+
+                // MEMO: If you want to show as failed, throw a exception here.
+                // throw new Exception("Something went wrong!");
+            });
+
+            Spinner.Start("Stage 1...", spinner =>
+            {
+                Thread.Sleep(1000 * 3);
+                spinner.Text = "Stage 2...";
+                Thread.Sleep(1000 * 3);
+                spinner.Fail("Something went wrong!");
+            });
+        }
+
+        static async Task AsyncDemo()
+        {
+            await Spinner.StartAsync("Processing...", async () =>
+            {
+                await Task.Delay(1000 * 3);
+
+                // MEMO: If you want to show as failed, throw a exception here.
+                // throw new Exception("Something went wrong!");
+            });
+
+            await Spinner.StartAsync("Stage 1...", async spinner =>
+            {
+                await Task.Delay(1000 * 3);
+                spinner.Text = "Stage 2...";
+                await Task.Delay(1000 * 3);
+                spinner.Fail("Something went wrong!");
+            });
+        }
+    }
+}
---
+++ Kurukuru/FirstDemo/publish_run.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+dotnet publish -c Release -r linux-x64 -f net5.0
+
+./bin/Release/net5.0/linux-x64/publish/FirstDemo
