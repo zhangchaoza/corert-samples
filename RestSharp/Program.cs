c7f385c0cbd752e3a52aad8af0b2c616ad98041a
zhangchao <zackability@hotmail.com>
2022-02-09 18:19:58 +0800

    feat: add restsharp demo

---
+++ RestSharp/Program.cs
@@ -0,0 +1,31 @@
+﻿using RestSharp;
+
+var options = new RestClientOptions("https://api.muxiaoguo.cn/api/bad_or_luck")
+{
+    ThrowOnAnyError = true,
+    Timeout = 1000
+};
+var client = new RestClient(options);
+
+var request = new RestRequest()
+    .AddQueryParameter("Num", "11111111111");
+
+var response = await client.PostAsync<MyResponse>(request, CancellationToken.None);
+
+Console.WriteLine(response?.msg);
+Console.WriteLine(response?.data?.analysis);
+Console.WriteLine(response?.data?.badORluck);
+
+public class MyResponse
+{
+    public int code { get; set; }
+    public string? msg { get; set; }
+    public data? data { get; set; }
+}
+
+public class data
+{
+    public string? fraction { get; set; }
+    public string? analysis { get; set; }
+    public string? badORluck { get; set; }
+}
