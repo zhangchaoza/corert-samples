657ef0e60695eb510d33e952634283ff76503b14
zhangchao <zackability@hotmail.com>
2022-03-11 17:53:41 +0800

    feat: add Flurl.Http demo

---
+++ Flurl.Http/FirstDemo/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Text.Json.Serialization;
+using Flurl;
+using Flurl.Http;
+
+var response = await "https://api.muxiaoguo.cn"
+    .AppendPathSegment("api/bad_or_luck")
+    .SetQueryParams(new { Num = "11111111111" })
+    .GetJsonAsync<MyResponse>();
+
+Console.WriteLine(response?.Msg);
+Console.WriteLine(response?.Data?.Analysis);
+Console.WriteLine(response?.Data?.BadORluck);
+
+public class MyResponse
+{
+    [JsonPropertyName("code")]
+    public int Code { get; set; }
+
+    [JsonPropertyName("msg")]
+    public string? Msg { get; set; }
+
+    [JsonPropertyName("data")]
+    public Data? Data { get; set; }
+}
+
+public class Data
+{
+    [JsonPropertyName("fraction")]
+    public string? Fraction { get; set; }
+
+    [JsonPropertyName("analysis")]
+    public string? Analysis { get; set; }
+
+    [JsonPropertyName("badORluck")]
+    public string? BadORluck { get; set; }
+}
---
+++ Flurl.Http/FirstDemo/publish_run.ps1
@@ -0,0 +1,3 @@
+﻿dotnet.exe publish -c Release -r win-x64 -f net6.0 --sc
+
+.\bin\Release\net6.0\win-x64\publish\FirstDemo.exe
