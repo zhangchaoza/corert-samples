6be0765e0090b1e400cfa05e98b9706ed2c0dba1
zhangchao <zackability@hotmail.com>
2022-03-14 16:12:34 +0800

    feat: add Spectre.Console readme

---
+++ Spectre.Console/README.md
@@ -0,0 +1,3 @@
+# README.md
+
+corert cannot get MetadataToken,`Spectre.Console.Cli.Demo2` do not works.
---
+++ Spectre.Console/Spectre.Console.Cli.Demo2/Program.cs
@@ -0,0 +1,62 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using Spectre.Console;
+using Spectre.Console.Cli;
+
+var app = new CommandApp();
+
+app.Configure(config =>
+{
+    config.AddBranch<AddSettings>("add", add =>
+    {
+        add.AddCommand<AddPackageCommand>("package");
+        add.AddCommand<AddReferenceCommand>("reference");
+    });
+
+    config.SetExceptionHandler(ex =>
+    {
+        AnsiConsole.WriteException(ex, ExceptionFormats.ShortenEverything);
+        return -99;
+    });
+});
+
+return app.Run(args);
+
+
+public class AddSettings : CommandSettings
+{
+    [CommandArgument(0, "[PROJECT]")]
+    public string? Project { get; set; }
+}
+
+public class AddPackageSettings : AddSettings
+{
+    [CommandArgument(0, "<PACKAGE_NAME>")]
+    public string? PackageName { get; set; }
+
+    [CommandOption("-v|--version <VERSION>")]
+    public string? Version { get; set; }
+}
+
+public class AddReferenceSettings : AddSettings
+{
+    [CommandArgument(0, "<PROJECT_REFERENCE>")]
+    public string? ProjectReference { get; set; }
+}
+
+public class AddPackageCommand : Command<AddPackageSettings>
+{
+    public override int Execute([NotNull] CommandContext context, [NotNull] AddPackageSettings settings)
+    {
+        // Omitted
+        return 0;
+    }
+}
+
+public class AddReferenceCommand : Command<AddReferenceSettings>
+{
+    public override int Execute([NotNull] CommandContext context, [NotNull] AddReferenceSettings settings)
+    {
+        // Omitted
+        return 0;
+    }
+}
