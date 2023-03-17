5953f1d9bfc5968092804f0b5966e532b6f898bf
zhangchao <zackability@hotmail.com>
2023-03-17 16:44:18 +0800

    feat: add Pastel

---
+++ PastelDemo/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace PastelDemo
+{
+    using System;
+    using System.Drawing;
+    using System.Linq;
+    using Pastel;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            "ENTER".Pastel(Color.FromArgb(165, 229, 250));
+            Console.WriteLine($"Press {"ENTER".Pastel(Color.FromArgb(165, 229, 250))} to continue");
+
+            var spectrum = new (string color, string letter)[]
+            {
+                ("#124542", "a"),
+                ("#185C58", "b"),
+                ("#1E736E", "c"),
+                ("#248A84", "d"),
+                ("#20B2AA", "e"),
+                ("#3FBDB6", "f"),
+                ("#5EC8C2", "g"),
+                ("#7DD3CE", "i"),
+                ("#9CDEDA", "j"),
+                ("#BBE9E6", "k")
+            };
+            Console.WriteLine(string.Join("", spectrum.Select(s => s.letter.Pastel(s.color))));
+
+            Console.WriteLine("Colorize me".Pastel(Color.Black).PastelBg("FFD000"));
+        }
+    }
+}
