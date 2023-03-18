4311e38899b523fb7180d096b0ae1b3924655f29
zhangchao <zackability@hotmail.com>
2023-03-19 00:17:03 +0800

    feat: add mqttnet

---
+++ MQTTnet/Client/Program.cs
@@ -0,0 +1,30 @@
+﻿using MQTTnet;
+using MQTTnet.Client;
+
+var optionBuilder = new MqttClientOptionsBuilder()
+    .WithClientId($"{Guid.NewGuid()}")
+    .WithCredentials("mySecretUser", "mySecretPassword")
+    .WithTcpServer("127.0.0.1", 1883)
+    .WithCleanSession();
+var options = optionBuilder.Build();
+
+var factory = new MqttFactory();
+var mqttClient = factory.CreateMqttClient();
+mqttClient.ApplicationMessageReceivedAsync += e =>
+{
+    Console.WriteLine("{0} {1} {2}", e.ClientId, e.ApplicationMessage.Topic, e.ApplicationMessage.Payload.Length);
+    return Task.CompletedTask;
+};
+
+await mqttClient.ConnectAsync(options, CancellationToken.None);
+await mqttClient.SubscribeAsync(new MqttClientSubscribeOptionsBuilder()
+    .WithTopicFilter(new MqttTopicFilterBuilder()
+        .WithTopic("my/topic"))
+    .Build(), CancellationToken.None);
+await mqttClient.PublishAsync(new MqttApplicationMessageBuilder()
+    .WithTopic("my/topic")
+    .WithPayload("hello world")
+    .Build(), CancellationToken.None);
+
+await Task.Delay(5000);
+await mqttClient.DisconnectAsync(new MqttClientDisconnectOptions(), CancellationToken.None);
---
+++ MQTTnet/Server/Program.cs
@@ -0,0 +1,26 @@
+﻿// Configure MQTT server.
+using System.Net;
+using MQTTnet;
+using MQTTnet.Server;
+
+var serverOptionsBuilder = new MqttServerOptionsBuilder()
+    .WithoutDefaultEndpoint()
+    .WithDefaultEndpoint()
+    .WithDefaultEndpointBoundIPAddress(IPAddress.Any)
+    .WithDefaultEndpointPort(1883);
+var serverOptions = serverOptionsBuilder.Build();
+
+var mqttFactory = new MqttFactory();
+var mqttServer = mqttFactory.CreateMqttServer(serverOptions);
+mqttServer.InterceptingPublishAsync += e =>
+{
+    Console.WriteLine("{0} {1}", e.ClientId, e.ApplicationMessage.Topic);
+    return Task.CompletedTask;
+};
+await mqttServer.StartAsync();
+
+var taskCompletion = new System.Threading.Tasks.TaskCompletionSource<int>();
+Console.CancelKeyPress += (s, e) => taskCompletion.SetResult(0);
+await taskCompletion.Task;
+await mqttServer.StopAsync();
+await Task.Delay(2000);
